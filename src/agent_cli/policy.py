686862f8e80f41c732ca8f0f7fa01100e319572b
Akayou Adane Kitessa <akuadane1205@gmail.com>
2026-05-01 22:12:38 -0400

    Added policy.py to request permission from the user when it is required.
    Moved files around for better clarity.

---
+++ src/agent_cli/policy.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style
+
+def ask_tool_permission(tool_name: str, arguments: dict) -> bool:
+    print(Style.RESET_ALL, flush=True)
+    print('\n', flush=True)
+    print(
+        Fore.RED
+        + f"System: Do you want to approve the use of {tool_name} with arguments: {arguments}?"
+        + Style.RESET_ALL,
+    )
+    user_input = input("> (y/n) ").lower()
+    if user_input == "y" or user_input == "yes":
+        return True
+    else:
+        return False
--- src/agent_core/tools/executor.py
+++ src/agent_core/tools/executor.py
@@ -1,13 +1,17 @@
-from src.agent_core.tools.tool import Tool
+from src.agent_core.tools.tool import Tool, ToolPermission
 from src.agent_core.providers.base import ToolResultMessage
-from typing import Generator
+from typing import Callable, Generator
 
 
 def sequential_executor(
-    tools: list[(Tool, dict)],
+    tools: list[(Tool, dict)], ask_tool_permission: Callable[[str, dict], bool]
 ) -> Generator[ToolResultMessage, None, None]:
 
     for tool, kwargs in tools:
+        if tool.permission == ToolPermission.HIGH:
+            if not ask_tool_permission(tool.name, kwargs):
+                yield ToolResultMessage(tool_name=tool.name, content=f"Tool {tool.name} was not allowed to be used by the user.")
+                continue 
         result = tool.execute(**kwargs)
         yield ToolResultMessage(tool_name=tool.name, content=str(result))
 
